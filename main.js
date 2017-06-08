77a205295ec973e4ad053d92dd5cdd1409a96d81
kopytar <kopitar71@gmail.com>
2017-06-08 15:42:46 +0200

    - updated some data that has changed - added "Open CDN source..." to the menu - minor fixes

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var Commands        = brackets.getModule("command/Commands"),
     Menus           = brackets.getModule("command/Menus"),
     EditorManager   = brackets.getModule("editor/EditorManager"),
     ExtensionUtils  = brackets.getModule("utils/ExtensionUtils"),
-    FileUtils  = brackets.getModule("file/FileUtils"),
+    FileUtils       = brackets.getModule("file/FileUtils"),
     Dialogs         = brackets.getModule("widgets/Dialogs");
     
 // get extension path, create dataObjectbject with all data from our JSON file
@@ -85,13 +85,21 @@ function constructMenu() {
         //menu.addMenuItem(window[menuVariable]);
     });
 }
+ 
+/* opens the JSON file with all the CDN data
+--------------------------------------------
+   NOTICE: editing sources.json must be done without any mistakes. A single typo can cause the whole extension to go nuts because our 'quickLib' menu is 
+           constructed and commands registered in a function with data from sources.json.                                                             */
+function openJSON() {
+    CommandManager.execute(Commands.CMD_ADD_TO_WORKINGSET_AND_OPEN, {fullPath: localPath + "sources.json", paneId: "first-pane"});
+}     
     
 // Menus -> quickLib : display the "About Extension" modal
-function about_quickLib() {
+function aboutModal() {
     var displayAbout = "<img style=\"float: left; margin:11px 5px 0px 0px; padding:0;\" src=\"styles/images/brackets_icon.svg\" alt=\"logo\" width=\"20\" height=\"20\">";
-    displayAbout += "<h3 style=\"margin-bottom:-5px;\">quickLib</h3></span>\n<small>version: 0.9.0</small><br><br>\n";
+    displayAbout += "<h3 style=\"margin-bottom:-5px;\">quickLib</h3></span>\n<small>version: 0.9.1</small><br><br>\n";
     displayAbout += "<span style=\"letter-spacing: 1px;\">Quick & simple adding all of those essential resources and snippets on Google Hosted Libraries.<hr>";
-    displayAbout += "<p>&#1023; Author: Kopitar Anže</p><p>&#1023; Homepage: <a href=\"https://github.com/kopytar/brackets-quicklib\" >https://github.com/kopytar/brackets-quicklib</a></p>";
+    displayAbout += "<p>&#1023; Author: Kopitar Anže</p><p>&#1023; Homepage: <a href=\"https://github.com/kopitar/brackets-quicklib\" >https://github.com/kopitar/brackets-quicklib</a></p>";
     displayAbout += "&#1023; Contact: kopitar71@gmail.com<br><hr>";
     // show modal dialog with "About Extension" information
     Dialogs.showModalDialog('a',"About Extension", displayAbout);
@@ -106,17 +114,23 @@ var MNU_COMMENTS = CommandManager.register('Add Comment', "comments.quicklib", f
 this.setChecked(!this.getChecked());
 isEnabled = this.getChecked();       
 });
+    
+// menu option for opening sources.json
+var MNU_JSON = "openjson.quicklib";
+CommandManager.register("Open CDN source...", MNU_JSON, openJSON );    
 
 // About Extension menu item
 var MNU_ABOUT = "about.quicklib";
-CommandManager.register("About Extension", MNU_ABOUT, about_quickLib );
+CommandManager.register("About Extension", MNU_ABOUT, aboutModal );
+console.log(CommandManager)
                
-// construct the menu for this extension, show it & register all commands    
+// construct the menu for this extension, register all commands & show it
 constructMenu();
 menu.addMenuDivider();
 // divider -> Enable/Disable comment prepending -> About Extension    
 menu.addMenuItem(MNU_COMMENTS);
 MNU_COMMENTS.setChecked(isEnabled);
+menu.addMenuItem(MNU_JSON);    
 menu.addMenuItem(MNU_ABOUT);     
 
 });
--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@ quickLib
 
  Brackets extension that adds simple access to all those essential resources on Google Hosted Libraries for fast snippet insertion.
  ( https://developers.google.com/speed/libraries/ )
+
+
+<img src="https://badges.ml/brackets-quicklib/total.svg?do_not_track" alt="total downloads of brackets-quicklib" class="hoverable"><img src="https://badges.ml/brackets-quicklib/last-version.svg?do_not_track" alt="last-version downloads of brackets-quicklib" class="hoverable">
